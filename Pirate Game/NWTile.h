c27eda2bc856159e4617e5aa31a452e65d5b37ff
Nicholas Wargnier <nwargnier@gmail.com>
2013-10-15 14:20:00 -0700

    Actions on each tile

--- Pirate Game/NWTile.h
+++ Pirate Game/NWTile.h
@@ -7,12 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "NWWeapon.h"
+#import "NWArmor.h"
 
 @interface NWTile : NSObject
 
 @property (strong, nonatomic) NSString *story;
 @property (strong, nonatomic) UIImage *backgroundImage;
 @property (strong, nonatomic) NSString *actionButtonName;
+@property (strong, nonatomic) NWWeapon *weapon;
+@property (strong, nonatomic) NWArmor *armor;
+@property (nonatomic) int healhEffect;
 
 
 @end
